3f2e3da739dead4d564a91f3a4f635e886cc86b8
hexivine <hexivine@gmail.com>
2026-05-22 01:39:16 +0530

    feat: add AppConfig constants

---
+++ lib/config.dart
@@ -0,0 +1,7 @@
+/// App configuration constants.
+class AppConfig {
+  static const String appName = 'Volt Rush';
+  static const String version = '1.2.0';
+  static const int maxRetries = 3;
+  static const Duration timeout = Duration(seconds: 30);
+}
