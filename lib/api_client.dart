1a298085b33e51648b1e2a44acc7a62db1206cde
bharathk-sps <96775260+bharathk-sps@users.noreply.github.com>
2026-05-25 18:13:17 +0530

    feat: add api client

---
+++ lib/api_client.dart
@@ -0,0 +1,8 @@
+﻿class ApiClient {
+  static const token = 'ghp_abc123secrettoken456';
+  
+  Future<String> fetchData(String url) async {
+    final response = await http.get(Uri.parse(url));
+    return response.body;
+  }
+}
